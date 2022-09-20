135b6a9919aca2add31dc7206491e46d2568e08a
Denisseortizu <denisseortizu@gmail.com>
2022-09-20 12:42:40 -0500

    New 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-
+require('dotenv').config();
+console.log(process.env);
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -12,14 +13,26 @@ app.listen(port, () => {
 });
 
 app.get('/dinoname', async (request, response) => {
-
 	const fetchApi = await fetch('https://dinoipsum.com/api/?format=json&words=2&paragraphs=1');
-    //.then(response => response.json())
-    //.then(data => console.log(data))
-    //.catch(err => console.error('Where did all the dinosaurs go?'))
 	const dinoNameResponse = await fetchApi.json();
 	console.log(dinoNameResponse);
 	response.json(dinoNameResponse[0]);
-  //console.log(response);
+});
+
+app.get('/dinoimage', async (request, response) => {
+
+	const fetchApi = await fetch(
+		'https://bing-image-search1.p.rapidapi.com/images/search?q=dinosaur&count=20',
+		{
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': process.env.API_KEY,
+		    'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
+			},
+		}
+	);
+	const dinoImageResponse = await fetchApi.json();
+	console.log(dinoImageResponse);
+	response.json(dinoImageResponse);
 });
 
--- public/script.js
+++ public/script.js
@@ -2,7 +2,9 @@
 console.log('script.js Se cargo :)');
 //import fetch from 'node-fetch';
 
-getDinoName();
+document.querySelector('#btnLoad').addEventListener('click', () => {
+	getDinoName();
+});
 
 
 async function getDinoName(){
@@ -10,4 +12,28 @@ async function getDinoName(){
 	const data = await response.json();
 	let dinoName = data.join(' ');
 	console.log(dinoName);
+	document.querySelector('#dinoName').textContent = dinoName;
+	getDinoImage();
+} 
+
+async function getDinoImage(){
+    const response = await fetch('/dinoimage');
+	const data = await response.json();
+	let dinoImage = data.value[Math.floor(Math.random() * data.value.length)];
+	let dinoImageURL = dinoImage.thumbnailUrl
+	let dinoImageAlt = dinoName;
+	console.log(dinoImage);
+
+	//Si ya hay una imagen existente, borrarla
+	if(document.querySelector('#dinoImage')){
+		document.querySelector('#dinoImage').remove();
+	}
+
+
+	//Create element image
+	let img = document.createElement('img');
+	img.id = 'dinoImage'
+	img.src = dinoImageURL;
+	img.alt = dinoImageAlt;
+	document.getElementById('body').appendChild(img);
 } 
