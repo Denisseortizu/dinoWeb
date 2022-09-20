72fcbd5a6d523e8ec94d2e9fcc9c3a8287e82bfb
Denisseortizu <denisseortizu@gmail.com>
2022-09-19 22:52:28 -0500

    Master

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 app.use(express.static('public'));
 
@@ -8,15 +11,15 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
-app.get('/dinoname', async(request, response)=> {
-    //runn code stuff
-    // Using Fetch API
-    const fetchApi = await fetch('https://dinoipsum.com/api/?format=json&words=2&paragraphs=1')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error('Where did all the dinosaurs go?'));
+app.get('/1', async (request, response) => {
 
-    const dinoNameResponse = await fetchApi.json();
-    console.log(dinoNameResponse);
-    response.json(dinoNameResponse);
+	const fetchApi = await fetch('https://dinoipsum.com/api/?format=json&words=2&paragraphs=1');
+    //.then(response => response.json())
+    //.then(data => console.log(data))
+    //.catch(err => console.error('Where did all the dinosaurs go?'))
+	const dinoNameResponse = await fetchApi.json();
+	console.log(dinoNameResponse);
+	response.json(dinoNameResponse[0]);
+  //console.log(response);
 });
+
--- public/script.js
+++ public/script.js
@@ -1,1 +1,15 @@
-console.log('script.js loaded');
+import fetch from 'node-fetch';
+console.log('script.js Se cargo :)');
+
+console.log('Hola');
+getDinoName();
+
+console.log('No se cargó funcion :(');
+
+async function getDinoName(){
+    console.log('Se carga funcion <3');
+    const response = await fetch('/');
+	const data = await response.json();
+	let dinoName = data[0].join(' ');
+	console.log(dinoName);
+} 
