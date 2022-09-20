e3f678586d58cb55461a5c471f37d21e8ae2bbf9
Denisseortizu <denisseortizu@gmail.com>
2022-09-20 15:36:37 -0500

    Ya casi final 1

--- public/script.js
+++ public/script.js
@@ -3,6 +3,17 @@ console.log('script.js Se cargo :)');
 //import fetch from 'node-fetch';
 
 document.querySelector('#btnLoad').addEventListener('click', () => {
+
+	//Si ya hay una imagen existente, borrarla
+	if(document.querySelector('#dinoName')){
+		document.querySelector('#dinoName').remove();
+	}
+
+	//Si ya hay una imagen existente, borrarla
+	if(document.querySelector('#dinoImage')){
+		document.querySelector('#dinoImage').remove();
+	}
+	getDinoImage();
 	getDinoName();
 });
 
@@ -12,8 +23,13 @@ async function getDinoName(){
 	const data = await response.json();
 	let dinoName = data.join(' ');
 	console.log(dinoName);
-	document.querySelector('#dinoName').textContent = dinoName;
-	getDinoImage();
+
+	//Creamos div
+	let dinoNameDiv = document.createElement('div');
+	dinoNameDiv.id = 'dinoName';
+	dinoNameDiv.textContent=dinoName;
+	document.querySelector('#dinoWraper').appendChild(dinoNameDiv);
+	
 } 
 
 async function getDinoImage(){
@@ -24,16 +40,11 @@ async function getDinoImage(){
 	let dinoImageAlt = dinoName;
 	console.log(dinoImage);
 
-	//Si ya hay una imagen existente, borrarla
-	if(document.querySelector('#dinoImage')){
-		document.querySelector('#dinoImage').remove();
-	}
-
 
 	//Create element image
 	let img = document.createElement('img');
 	img.id = 'dinoImage'
 	img.src = dinoImageURL;
 	img.alt = dinoImageAlt;
-	document.getElementById('body').appendChild(img);
+	document.querySelector('#dinoWraper').appendChild(img);
 } 
